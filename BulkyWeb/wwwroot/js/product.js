25cb007e53bf1d3f05e662b1073c746837af41fa
Priyanshu Singh <priyanshu.singh@avendata-group.com>
2024-05-25 19:00:20 +0530

    Add project files.

---
+++ BulkyWeb/wwwroot/js/product.js
@@ -0,0 +1,17 @@
+﻿
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url:'/admin/product/getall'},
+        "columns": [
+        { data: 'title', "widht": "25%" },
+        { data: 'isbn', "widht": "15%" },
+        { data: 'listPrice', "widht": "10%" },
+        { data: 'author', "widht": "20%" },
+        { data: 'category.name', "widht": "15%" }
+    ]
+    });
+}
