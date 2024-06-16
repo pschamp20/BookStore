859346d1ed0cc626c6d0b6c2f669e549da4c8365
Priyanshu Singh <priyanshu.singh@avendata-group.com>
2024-06-16 21:04:09 +0530

    Final Commit

---
+++ BookStore.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,25 @@
+﻿using BookStore.DataAccess.Repository.IRepository;
+using BookStore.Models;
+using BookStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DataAccess.Repository
+{
+	public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository 
+	{
+		private ApplicationDbContext _db;
+		public ApplicationUserRepository(ApplicationDbContext db) : base(db)
+		{ 
+			_db = db;
+		}
+		public void Update(ApplicationUser applicationUser)
+		{
+			_db.ApplicationUsers.Update(applicationUser);
+		}
+	}
+}
